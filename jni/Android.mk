d39151889fb903a140fe2158381a1c9c516b8f6b
jianglei12138 <jianglei12138@gmail.com>
2016-04-10 12:36:52 +0800

    add makefile and library

---
+++ jni/Android.mk
@@ -0,0 +1,27 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	blocksort.c    bzlib.c        crctable.c     huffman.c  \
+	compress.c     decompress.c   randtable.c
+
+LOCAL_MODULE := bz2
+
+LOCAL_INCLUDE_FILES := \
+	$(LOCAL_PATH)/
+
+include $(BUILD_SHARED_LIBRARY)
+
+
+
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	bzip2.c
+
+LOCAL_SHARED_LIBRARIES := bz2
+LOCAL_MODULE := bzip2
+
+include $(BUILD_EXECUTABLE)
+
